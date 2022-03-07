d0165a730d687a059f428810acfc0d017e38c60d
Teslim Abdulkareem <asleemy20@gmail.com>
2022-03-07 12:37:50 +0800

    practice python task

--- Coding challege/square_of_addition.py
+++ Coding challege/square_of_addition.py
@@ -1,8 +1,11 @@
-def add(a,b):
+first_number = int(input('Enter first number:\n'))
+second_number = int(input('Enter second number:\n'))
+
+def add(a, b):
     return a + b
 
 def square (c):
-    return c*c
+    return c * c
 
-result = square(add(5,7))
-print(result)
+result = square(add(first_number,second_number))
+print('Result is',result)
